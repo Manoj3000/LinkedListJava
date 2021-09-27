3d2d36618ec9352ecd9ea8804c5dbe205b89eab9
manoj gaikwad <gaikwadm853@gmail.com>
2021-09-27 13:26:29 +0530

    Added: create a simple linked list

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -1,8 +1,18 @@
 package com.bridgelabz.linkedlist;
 
+import java.util.LinkedList;
+
 public class LinkedListMain {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to Linked List Program.");
+		
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+		
+		linkedList.add(56);
+		linkedList.add(30);
+		linkedList.add(70);
+		
+		System.out.println(linkedList);
 	}
 }
