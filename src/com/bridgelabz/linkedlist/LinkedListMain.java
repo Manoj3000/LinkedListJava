dfc976d5e232483d7ef679d543915f8a622fd516
manoj gaikwad <gaikwadm853@gmail.com>
2021-09-27 13:21:59 +0530

    Added: message Welcome to Linked List Program.

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -1,17 +1,8 @@
 package com.bridgelabz.linkedlist;
 
-import java.util.LinkedList;
-
 public class LinkedListMain {
 
 	public static void main(String[] args) {
-
-		LinkedList<Integer> linkedList = new LinkedList<Integer>();
-
-		linkedList.add(56);
-		linkedList.add(30);
-		linkedList.add(70);
-		
-		System.out.println(linkedList);
+		System.out.println("welcome to Linked List Program.");
 	}
 }
