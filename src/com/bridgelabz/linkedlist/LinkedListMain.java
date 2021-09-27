ebb8df050a8bbec309fe45f7a75feb41eeaaa77e
manoj gaikwad <gaikwadm853@gmail.com>
2021-09-27 13:13:20 +0530

    Added: created a simple Linked List

---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+
+		linkedList.add(56);
+		linkedList.add(30);
+		linkedList.add(70);
+		
+		System.out.println(linkedList);
+	}
+}
